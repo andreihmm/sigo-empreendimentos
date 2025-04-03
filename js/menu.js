906c0026d51890e3725a63680f401d3d2432a705
Andrei Alves Fracalossi <124471216+andreihmm@users.noreply.github.com>
2025-04-02 21:14:47 -0300

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(() => { // Aguarda carregamento do header
+        const menuToggle = document.getElementById("menu-toggle");
+        const navMenu = document.getElementById("nav-menu");
+
+        if (menuToggle && navMenu) {
+            menuToggle.addEventListener("click", function () {
+                navMenu.classList.toggle("nav-active"); // Alterna a classe para abrir e fechar
+                menuToggle.classList.toggle("active"); // Alterna o efeito de "X"
+
+            });
+        } else {
+            console.error("Erro: Elementos 'menu-toggle' ou 'nav-menu' não encontrados.");
+        }
+    }, 500); // Pequeno delay para garantir que o header carregou
+});
